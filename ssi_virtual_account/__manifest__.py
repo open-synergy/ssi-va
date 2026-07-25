2ce3a592f43eb0d2c968d60aa0a64e335cf6d959
Andhitia Rama <andhitia.r@gmail.com>
2026-07-25 13:35:43 +0700

    [ADD] ssi_virtual_account
    Modul baru untuk master data Virtual Account (VA) biller. Menyediakan
    model va_biller (mixin.master_data) beserta child va_biller.code yang
    menyimpan kode VA per bank untuk tiap biller.
    
    * Model va_biller: master data biller penerima pembayaran VA.
    * Model va_biller.code: kode VA per bank untuk sebuah biller, dengan
      constraint mencegah duplikasi bank pada biller yang sama, dan
      name_get() kustom agar informatif saat dipilih lewat Many2one dari
      modul lain.
    * Security: group konfigurator va_biller_group, kategori module baru
      Virtual Account (root) + Virtual Account - Configurator (child).
    * Menu utama baru "Virtual Account" dengan submenu Configuration.
    * Unit test odoo-yaml-test mencakup CRUD va_biller, o2m bank_code_ids,
      cascade delete, constraint duplikasi bank, dan format display_name.
    
    Closes open-synergy/ssi-va#2

---
+++ ssi_virtual_account/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Virtual Account Management",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_master_data_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_groups/va_biller.xml",
+        "security/ir_model_access/va_biller.xml",
+        "menu.xml",
+        "views/va_biller.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_virtual_account/tests/test_va_biller.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from odoo_yaml_test import YamlTransactionCase
+
+
+@tagged("post_install", "-at_install")
+class TestVABiller(YamlTransactionCase):
+    def test_va_biller(self):
+        self.run_yaml_scenario("test_data_va_biller.yaml")
