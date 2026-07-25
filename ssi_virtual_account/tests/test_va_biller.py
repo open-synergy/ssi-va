90e8a2b515f47713f32561072fec006f619bb0d9
Andhitia Rama <andhitia.r@gmail.com>
2026-07-25 13:36:27 +0700

    [UPD] ssi_virtual_account
    * Tambahkan setup/ hasil setuptools-odoo-make-default (pre-commit hook).
    * Perbaiki urutan import di tests/test_va_biller.py hasil isort.

--- ssi_virtual_account/tests/test_va_biller.py
+++ ssi_virtual_account/tests/test_va_biller.py
@@ -2,9 +2,9 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import tagged
-
 from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
 
 
 @tagged("post_install", "-at_install")
